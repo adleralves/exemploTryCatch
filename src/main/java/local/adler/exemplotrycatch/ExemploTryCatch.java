4c67ec98097fd6a6fae85ef08e45482c64ac8b17
adleralves <adler.alves@hotmail.com>
2022-03-15 16:36:27 -0300

    Implementado TryCatch - v1.2

--- src/main/java/local/adler/exemplotrycatch/ExemploTryCatch.java
+++ src/main/java/local/adler/exemplotrycatch/ExemploTryCatch.java
@@ -6,15 +6,26 @@ public class ExemploTryCatch {
         
         Scanner t = new Scanner(System.in);
         
-        int[] matriz = new int[0];
+        int[] matriz = new int[2];
         int i;
+        boolean continuar = true;
         
-        System.out.println("Informe a posição do array que deseja selecionar: ");
-        i = t.nextInt();
-        
-        System.out.println("Informe o valor: ");
-        matriz[i] = t.nextInt();
-        
-        System.out.printf("Elemento %d: %d\n", i, matriz[i]);
+        do {
+            try { 
+                System.out.println("Informe a posição do array que deseja selecionar: ");
+                i = t.nextInt();
+
+                System.out.println("Informe o valor: ");
+                matriz[i] = t.nextInt();
+
+                System.out.printf("Elemento %d: %d\n", i, matriz[i]);
+                
+                continuar = false;
+            } catch (ArrayIndexOutOfBoundsException erro) {
+                System.err.printf("%nException: %s%n", erro);
+                t.nextLine();
+                System.out.println("!ERRO! - você selecionou um elemento não existente da matriz | por favor selecione até o elemento de n° 1 \n\n");
+            }
+        } while (continuar);
     }
 }
