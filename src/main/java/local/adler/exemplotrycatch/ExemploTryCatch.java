c136696a0f757a0c7dc9343312f93e625fd43570
adleralves <adler.alves@hotmail.com>
2022-03-15 14:00:15 -0300

    Implementado Array com erro "ArrayOutOfBounds" - v1.1

--- src/main/java/local/adler/exemplotrycatch/ExemploTryCatch.java
+++ src/main/java/local/adler/exemplotrycatch/ExemploTryCatch.java
@@ -1,7 +1,20 @@
 package local.adler.exemplotrycatch;
+import java.util.Scanner;
 
 public class ExemploTryCatch {
     public static void main(String[] args) {
         
+        Scanner t = new Scanner(System.in);
+        
+        int[] matriz = new int[0];
+        int i;
+        
+        System.out.println("Informe a posição do array que deseja selecionar: ");
+        i = t.nextInt();
+        
+        System.out.println("Informe o valor: ");
+        matriz[i] = t.nextInt();
+        
+        System.out.printf("Elemento %d: %d\n", i, matriz[i]);
     }
 }
